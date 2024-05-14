585d46eaae99bd9d5f00c19a6773117a4ab4c185
Lê Thiên Doanh <143641289+ltdoan2004@users.noreply.github.com>
2024-05-14 13:13:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import torch
 import utils
 from ultralytics import YOLO
 model = YOLO('./yolov8n.yaml').load('./yolov8n.pt')  
-results = model.train(data="./data.yaml", epochs=50, imgsz = 640)
+results = model.train(data="./data.yaml", epochs=150, imgsz = 640)
